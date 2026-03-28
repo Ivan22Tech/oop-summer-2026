e29ef85c92f8c14e9541763902c7abd65af6a434
Ivan22Tech <Ivanzakaria767@gmail.com>
2026-03-28 18:58:56 +0100

    Update test1.py

--- students/36479/test1.py
+++ students/36479/test1.py
@@ -1,5 +1,14 @@
-#this is my second test
-Print("hello world!")
-if x = 3 
- while b = 6 
-print(x.b)
+students = [
+    {"name": "Alex", "age": 21, "major": "CS"},
+    {"name": "John", "age": 22, "major": "Math"},
+    {"name": "Emma", "age": 20, "major": "Physics"},
+    {"name": "Liam", "age": 23, "major": "Engineering"}
+]
+
+# Access examples
+print(students[0]["name"])  # Alex
+print(students[1]["major"])  # Math
+
+# Loop through all students
+for student in students:
+    print(student["name"], student["age"], student["major"])
